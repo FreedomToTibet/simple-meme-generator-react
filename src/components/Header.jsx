341573f03feb558209e86de84c5d59f88e3a18fa
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2022-11-06 15:01:47 +0100

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+			<img src="./img/Troll_Face.png" alt="Troll Face" className="header--image" />
+      <h2 className="header--title">Meme Generator</h2>
+    </header>
+  );
+};
+
+export default Header;
